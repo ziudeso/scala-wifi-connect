5f21f6616cb5ad44ef22e9a26d3923fd49b26b26
Francesco Delogu <ziudeso@gmail.com>
2020-06-23 16:12:03 +0200

    ref

--- ui/build/service-worker.js
+++ ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a619d5c0df1bb38186cdf977820ec568.js"
+  "/precache-manifest.4b2dac897cb361925df0b9680f649318.js"
 );
 
 self.addEventListener('message', (event) => {
